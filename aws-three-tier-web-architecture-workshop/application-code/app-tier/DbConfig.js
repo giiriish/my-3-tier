2a1e87bcb86ec216674b9ea85f6cba8e8b9e723a
giiriish <girishlatake2005@gmail.com>
2026-02-28 02:35:04 +0530

    Initial 3-tier web architecture project

---
+++ aws-three-tier-web-architecture-workshop/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-2.cdswy00swkac.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'guru4545',
+    DB_DATABASE : 'webappdb'
+});
